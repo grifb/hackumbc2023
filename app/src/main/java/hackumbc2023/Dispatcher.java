abc9205c536833278a0c81ea4c7323d46205af3f
Asher Edwards <ashertetko@gmail.com>
2023-09-23 16:08:12 -0400

    Update Dispatcher.java

--- app/src/main/java/hackumbc2023/Dispatcher.java
+++ app/src/main/java/hackumbc2023/Dispatcher.java
@@ -3,7 +3,11 @@ package hackumbc2023;
 public class Dispatcher extends Driver {
     public Dispatcher() {
     }
-    
+
+    public Dispatcher(String name, String number, boolean is21){
+        super(name,number,is21);
+    }
+
     public void postShift() {
     }
 }
