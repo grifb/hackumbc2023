9a3597c0a1e5623f1eaab44b7064b4d9792e966b
Asher Edwards <ashertetko@gmail.com>
2023-09-23 14:22:48 -0400

    Update Dispatcher.java

---
+++ app/src/main/java/hackumbc2023/Dispatcher.java
@@ -0,0 +1,17 @@
+package hackumbc2023;
+
+public class Dispatcher{
+    public Dispatercher() {
+    }
+    private void PostShift(){
+
+    }
+    protected void DropShift(){
+
+    }
+    public void ViewShift() {
+    }
+
+    public void Schedule() {
+    }
+}
