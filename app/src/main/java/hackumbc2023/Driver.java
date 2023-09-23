04b7dbbf37e0662ca4cf7d2b4c6d14df89691be5
grifb <114772878+grifb@users.noreply.github.com>
2023-09-23 16:18:04 -0400

    fix: brought back Driver.java

---
+++ app/src/main/java/hackumbc2023/Driver.java
@@ -0,0 +1,29 @@
+package hackumbc2023;
+
+public class Driver {
+	String number;
+	String name;
+	boolean is21;
+	String test;
+	
+    public Driver(String name, String number, boolean is21) {
+    	this.name = name;
+    	this.number = number;
+    			
+    }
+
+    public void getNumber() {
+    }
+
+    public void setNumber() {
+    }
+
+    public void unassign() {
+    }
+
+    public void dropShift() {
+    }
+
+    public void viewShifts() {
+    }
+}
