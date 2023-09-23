87d86104b96200c8998c95364e3a2668ac42651d
C4 Flame <jacquicannady@gmail.com>
2023-09-23 16:28:55 -0400

    Update Driver.java

--- app/src/main/java/hackumbc2023/Driver.java
+++ app/src/main/java/hackumbc2023/Driver.java
@@ -1,22 +1,39 @@
-package app.src.main.java.hackumbc2023;
+package hackumbc2023;
 
 public class Driver {
 	String number;
 	String name;
 	boolean is21;
-	String test;
+    DriverLevel level;
 	
-    public Driver(String name, String number, boolean is21) {
+    public Driver(String name, String number, boolean is21, DriverLevel level) {
     	this.name = name;
     	this.number = number;
-    			
+        this.level = level;
     }
 
-    public void getNumber(){
-
+    public String getNumber(){
+        return number;
     }
 
-    public void setNumber(){
-        
+    public void setNumber(String newNumber){
+        this.number = newNumber;
     }
+
+    public DriverLevel getDriverLevel() {
+        return level;
+    }
+
+    public boolean getIs21() {
+        return is21;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
 }
