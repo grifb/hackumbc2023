6dc2aa2f2f22e70a6775bb4513dbfca353ec669f
C4 Flame <jacquicannady@gmail.com>
2023-09-23 16:48:39 -0400

    idk

--- app/src/main/java/hackumbc2023/MOD.java
+++ app/src/main/java/hackumbc2023/MOD.java
@@ -4,20 +4,23 @@ public class MOD {
     public MOD() {
     }
 
+
+    //edits a shift currently on the server
     public final void edit() {
     }
 
+    //assigns a shift to a driver
     public final void give() {
     }
 
+    //assigns a shift to a driver but doesnt return an error if shifts overlap
     public final void overlap() {
     }
 
+    //removes a shift from a driver
     public void unassign() {
     }
 
-    public void dropShift() {
-    }
 
     public void postShift() {
     }
